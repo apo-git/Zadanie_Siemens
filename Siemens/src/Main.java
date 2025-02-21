0ddbada39e9f22892915de0c484b0e18959ab313
Adam Melničák <adam.melnicak.2@student.tuke.sk>
2025-02-21 13:10:08 +0100

    Presunuty kod z main metody do vlastnej metody na generovanie cisla, kontrola +/- pred if/else

--- Siemens/src/Main.java
+++ Siemens/src/Main.java
@@ -5,39 +5,34 @@ public class Main {
         Random random = new Random();
 
         for (int length = 1; length <= 50; length++) {
-            //int len = 1;
+            boolean isNegative = random.nextInt(2) == 1;
 
-            boolean isNegative = (random.nextInt(2) == 1);
+            String number = generateRandomString(length, isNegative, random);
 
-
-            if (!isNegative) {
-                char[] number = new char[length];
-
-                number[0] = (char) ('1' + random.nextInt(9));
-
-                for (int i = 1; i < length; i++) {
-                    number[i] = (char) ('0' + random.nextInt(10));
-                    //len++;
-                }
-
-                System.out.println(new String(number));
-            } else {
-                char[] number = new char[length + 1];
-                int len = 1;
-                number[0] = '-';
-                number[1] = (char) ('1' + random.nextInt(9));
-
-                for (int i = 2; i < length + 1; i++) {
-                    number[i] = (char) ('0' + random.nextInt(10));
-                    len++;
-                }
-
-                System.out.println(new String(number));
-                System.out.println(len);
-            }
-
-            //System.out.println(len);
+            System.out.println(number);
         }
+    }
+
+    public static String generateRandomString(int length, boolean isNegative, Random random) {
+        int extraCharacterIfNegative;
+
+        if (isNegative) {
+            extraCharacterIfNegative = 1;
+        } else {
+            extraCharacterIfNegative = 0;
+        }
+
+        char[] number = new char[length + extraCharacterIfNegative];
+
+        if (isNegative) number[0] = '-';
+
+        number[extraCharacterIfNegative] = (char) ('1' + random.nextInt(9));
+
+        for (int i = extraCharacterIfNegative + 1; i < number.length; i++) {
+            number[i] = (char) ('0' + random.nextInt(10));
+        }
+
+        return new String(number);
     }
 }
 
