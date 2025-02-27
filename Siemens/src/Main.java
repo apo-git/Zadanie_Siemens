307d20adc865a23ede8b6ff7e917383446296045
Adam Melničák <adam.melnicak.2@student.tuke.sk>
2025-02-27 23:55:55 +0100

    Prepared decision maker for add/sub method for numbers

--- Siemens/src/Main.java
+++ Siemens/src/Main.java
@@ -42,6 +42,71 @@ public class Main {
         // returning generated string
         return new String(number);
     }
+
+    // prepared for final part
+    public static String addOrSubstractString(String number1, String number2) {
+        int negative1 = 0;
+        int negative2 = 0;
+
+        if (number1.startsWith("-")) {
+            negative1 = 1;
+        }
+
+        if (number2.startsWith("-")) {
+            negative2 = 1;
+        }
+
+        String absolute1;
+        if (negative1 == 1) absolute1 = number1.substring(1);
+        else absolute1 = number1;
+
+        String absolute2;
+        if (negative2 == 1) absolute2 = number2.substring(1);
+        else absolute2 = number2;
+
+        if (number1.charAt(0) == '-' && number2.charAt(0) == '-') {
+            return "- add+";
+        } else if (number1.charAt(0) != '-' && number2.charAt(0) != '-') {
+            return "+ add+";
+        } else {
+            int subHelp = compare(absolute1, absolute2);
+
+            if (subHelp == 0) {
+                return "0";
+            } else if (subHelp > 0) {
+                return "sub(a,b)"; // repair this part of code
+                // need implementation
+            } else {
+                return "sub(b,a)"; // repair this part of code
+                // need implementation
+            }
+        }
+    }
+
+    public static int compare(String absolute1, String absolute2) {
+        int lenOfNum1 = absolute1.length();
+        int lenOfNum2 = absolute2.length();
+        int subHelp = 0;
+
+        if (lenOfNum1 != lenOfNum2) {
+            subHelp = lenOfNum1 - lenOfNum2;
+        } else {
+            for (int i = 0; i < lenOfNum1; i++) {
+                int digit1 = absolute1.charAt(i) - '0';
+                int digit2 = absolute2.charAt(i) - '0';
+                if (digit1 > digit2) {
+                    subHelp = 1;
+                    break;
+                }
+                if (digit1 < digit2) {
+                    subHelp = -1;
+                    break;
+                }
+            }
+        }
+
+        return subHelp;
+    }
 }
 
 
